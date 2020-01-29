98e676b7ad5783420c9743b2458850f705e8d308
Dani Matton <danielle@Danis-MacBook-Pro.local>
2020-01-29 16:45:28 -0700

    Bomb and imageslider components working

--- src/Bomb.js
+++ src/Bomb.js
@@ -1,1 +1,21 @@
-// your Bomb code here!
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+class Bomb extends Component {
+    
+    state = {
+        secondsLeft: this.props.initialCount
+    }
+    
+    render() {
+        return(
+            <div>
+               {this.state.secondsLeft == 0 
+               ? <h2>Boom!</h2> 
+               : <h2>{this.state.secondsLeft} seconds left before I go boom!</h2>}
+            </div>
+        )
+    }
+}
+
+export default Bomb;
--- src/ImageSlider.js
+++ src/ImageSlider.js
@@ -1,1 +1,18 @@
-// your ImageSlider code here!
+import React, { Component } from 'react'
+
+class ImageSlider extends Component {
+
+state = {
+    currentSlideIndex: 0
+}
+
+    render() {
+        return(
+            <div>
+                <h2>I am on slide {this.state.currentSlideIndex}</h2>
+            </div>
+        )
+    }
+}
+
+export default ImageSlider;
